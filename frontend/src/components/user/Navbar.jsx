814d86dff2069e6d3af6f06d0038cc0f9d3ab75d
Netanel Shimoni <netanelsh0@gmail.com>
2022-08-21 22:23:34 +0300

    Add Edit profile & clear NavBar

--- frontend/src/components/user/Navbar.jsx
+++ frontend/src/components/user/Navbar.jsx
@@ -1,37 +1,60 @@
-import React from 'react';
-import { BsFillSunFill } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
-import { useAuth, useTheme } from '../../hooks';
-import Container from '../Container';
+import React from "react";
+import { BsFillSunFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import { useAuth, useTheme } from "../../hooks";
+import Container from "../Container";
 
 export default function Navbar() {
-    const {toggleTheme} = useTheme();
-    const {authInfo, handleLogout} = useAuth();
-    const {isLoggedIn} = authInfo;
-    return (
-        <div className="bg-secondary shadow-sm shadow-gray-500">
-                <div className="flex justify-between items-center">
-                    <Link to="/">
-                        <img src="./logo.jpg" alt="" className="h-6" />
-                    </Link>
-                    <ul className="flex items-center space-x-4"><li>
-                        <button onClick={toggleTheme} className="dark:bg-white bg-dark-subtle p-1 rounded">
-                            <BsFillSunFill className="text-secondary" size={24} />
-                        </button>
-                    </li>
-                        <li>
-                            <input type="text" className="border-2 border-dark-subtle p-1 rounded bg-transparent text-xl outline-none focus:border-white transition text-white" placeholder="Search..." />
-                        </li>
-                        <li>
-                            {isLoggedIn? (<button onClick={handleLogout} className="text-white font-semibold text-lg">
-                                Log out
-                                </button>):(<Link className="text-white font-semibold text-lg"
-                                 to="auth/signin">
-                                Login
-                            </Link>)}
-                        </li>
-                    </ul>
-                </div>
-        </div>
-    );
+  const { toggleTheme } = useTheme();
+  const { authInfo, handleLogout } = useAuth();
+  const { isLoggedIn } = authInfo;
+  return (
+    <div className="bg-secondary shadow-sm shadow-gray-500">
+      <div className="flex justify-between items-center pl-2">
+        <Link to="/">
+          <img src="./logo.jpg" alt="" className="h-8" />
+        </Link>
+        {/** TODO replace input to autoComplete**/}
+        <input
+          type="text"
+          className="border-2 border-dark-subtle p-1 rounded bg-transparent text-xl outline-none focus:border-white transition text-white "
+          placeholder="Search..."
+        />
+
+        <ul className="flex items-center space-x-4">
+          <li hidden={!isLoggedIn}>
+            <button className="text-white font-semibold text-lg">
+              {" "}
+              Edit Profile
+            </button>
+          </li>
+          <li className="pr-3">
+            {isLoggedIn ? (
+              <button
+                onClick={handleLogout}
+                className="text-white font-semibold text-lg"
+              >
+                Log out
+              </button>
+            ) : (
+              <Link
+                className="text-white font-semibold text-lg"
+                to="auth/signin"
+              >
+                Login
+              </Link>
+            )}
+          </li>
+          <li className=" pr-3">
+            <button
+              onClick={toggleTheme}
+              className="dark:bg-white bg-dark-subtle p-1 rounded "
+            >
+              <BsFillSunFill className="text-secondary" size={24} />
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
